1e6b0bcdf36bc27bfb6aeb7f861660e71c31a531
snowdence <snowdence2911@gmail.com>
2021-04-25 01:54:32 +0700

    [Release] - 1.0 View folder image success

--- WumBum/app/src/main/java/com/wifosoft/wumbum/activities/MainActivity.java
+++ WumBum/app/src/main/java/com/wifosoft/wumbum/activities/MainActivity.java
@@ -340,20 +340,7 @@ public class MainActivity extends SharedMediaActivity implements
         findViewById(R.id.nothing_to_show_placeholder).setVisibility(status ? View.VISIBLE : View.GONE);
     }
 
-    @Deprecated
-    public void checkNothing(boolean status) {
-        //TODO: @jibo come vuo fare qua? o anzi sopra!
-        ((TextView) findViewById(R.id.emoji_easter_egg)).setTextColor(getSubTextColor());
-        ((TextView) findViewById(R.id.nothing_to_show_text_emoji_easter_egg)).setTextColor(getSubTextColor());
 
-        if (status && Prefs.showEasterEgg()) {
-            findViewById(R.id.ll_emoji_easter_egg).setVisibility(View.VISIBLE);
-            findViewById(R.id.nothing_to_show_placeholder).setVisibility(View.VISIBLE);
-        } else {
-            findViewById(R.id.ll_emoji_easter_egg).setVisibility(View.GONE);
-            findViewById(R.id.nothing_to_show_placeholder).setVisibility(View.GONE);
-        }
-    }
 
     @Override
     protected void onStart() {
