7cb41bb6176d23117bcc1b8fc5c2f5f7fb157c41
snowdence <snowdence2911@gmail.com>
2021-05-15 01:39:33 +0700

    [+] Upgrade to new gradle and add ML

---
+++ WumBum/app/src/main/java/com/wifosoft/wumbum/PeopleRecognition.java
@@ -0,0 +1,14 @@
+package com.wifosoft.wumbum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PeopleRecognition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_people_recognition);
+    }
+}
