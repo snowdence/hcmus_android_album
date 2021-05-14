525ba556b8e2077302e50304a7acc5b9aaf9a8fd
Thái Ngô <hacbangtam@gmail.com>
2021-05-15 00:25:40 +0700

    [BUG] Fix app language (static)

--- WumBum/app/src/main/java/com/wifosoft/wumbum/activities/MainActivity.java
+++ WumBum/app/src/main/java/com/wifosoft/wumbum/activities/MainActivity.java
@@ -152,14 +152,6 @@ public class MainActivity extends SharedMediaActivity implements
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Locale locale = new Locale("vi-rVN");
-        Locale.setDefault(locale);
-        Resources resources = this.getResources();
-        Configuration config = resources.getConfiguration();
-        config.setLocale(locale);
-        resources.updateConfiguration(config, resources.getDisplayMetrics());
-
-
         setContentView(R.layout.activity_main);
         unbinder = ButterKnife.bind(this);
 
